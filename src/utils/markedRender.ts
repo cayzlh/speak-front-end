a741bcd6822efcde5d4736a5b5e5047592de9663
chenanyu <ant.chenanyu@icloud.com>
2023-02-08 10:19:19 +0800

    modidy

--- src/utils/markedRender.ts
+++ src/utils/markedRender.ts
@@ -4,9 +4,10 @@ import hljs from 'highlight.js'
 export const markedRender = (body, loading_img) => {
   const renderer = {
     image(href, title, text) {
-      return `<a href="${href}" target="_blank" data-fancybox="group" class="fancybox">
+      /*return `<a href="${href}" target="_blank" data-fancybox="group" class="fancybox">
            <img speak-src="${href}" src=${loading_img} alt='${text}'>
-          </a>`
+          </a>`*/
+      return `<img speak-src="${href}" src=${loading_img} alt='${text}'>`
     }
   }
   marked.setOptions({
